38569c697b6bbf6292de40fae97d86e9385780b5
nicholaskh <zhangkaihong3@wanda.cn>
2016-03-30 10:40:32 +0800

    remove unused code

--- app/models/service/user.lua
+++
@@ -1,9 +0,0 @@
-local table_dao = require('app.models.dao.table'):new()
-local UserService = {}
-
-function UserService:get()
-    table_dao:set('zhou', 'j')
-    return table_dao.zhou
-end
-
-return UserService
